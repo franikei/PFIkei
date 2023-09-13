36f8b25efac2b9bd231da1360cc8c8d9acf4c790
franikei <francoikei@gmail.com>
2023-09-13 20:56:26 -0300

    ultimo cambio

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     // Nombre y apellido del cliente
-    let nombre = obtenerTextoValido("Por favor, ingrese el nombre:");
+    let nombre = obtenerTextoValido("Por favor, ingrese el nombree:");
     let apellido = obtenerTextoValido("Por favor, ingrese el apellido:");
   
     // mensaje de bienvenida
